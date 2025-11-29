c0ef355ec94e2a9fe5c6ee7c63a941feb6801a29
Himanshu Singh <himanshusingh1k@gmail.com>
2025-11-29 21:38:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-const prevButton = document.querySelector("#prev");
-const nextButton = document.querySelector("#next");
 const circles = document.querySelectorAll(".circle");
 const lines = document.querySelectorAll(".line");
-let currentActive = 1;
-nextButton.addEventListener("click", () => {
-  currentActive++;
-  if (currentActive > circles.length) currentActive = circles.length;
-  updateUI();
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+
+let index = 0;
+
+nextBtn.addEventListener("click", () => {
+  if (index < lines.length) {
+    circles[index + 1].classList.add("active");
+    lines[index].classList.add("active-line");
+    prevBtn.classList.remove("disable");
+    prevBtn.removeAttribute("disabled");
+    index++;
+    if (index === lines.length) {
+      nextBtn.classList.add("disable");
+      nextBtn.setAttribute("disabled", "true");
+    }
+  }
 });
-prevButton.addEventListener("click", () => {
-  currentActive--;
-  if (currentActive < 1) currentActive = 1;
-  updateUI();
+
+prevBtn.addEventListener("click", () => {
+  if (index > 0) {
+    circles[index].classList.remove("active");
+    lines[index - 1].classList.remove("active-line");
+    nextBtn.classList.remove("disable");
+    nextBtn.removeAttribute("disabled");
+    index--;
+    if (index === 0) {
+      prevBtn.classList.add("disable");
+      prevBtn.setAttribute("disabled", "true");
+    }
+  }
 });
-function updateUI() {
-  circles.forEach((circle, index) => {
-    if (index < currentActive) circle.classList.add("active");
-    else circle.classList.remove("active");
-  });
-  lines.forEach((line, index) => {
-    if (index < currentActive - 1) line.classList.add("active-line");
-    else line.classList.remove("active-line");
-  });
-  if (currentActive === 1) prevButton.disabled = true;
-  else if (currentActive === circles.length) nextButton.disabled = true;
-  else (prevButton.disabled = false), (nextButton.disabled = false);
-}
